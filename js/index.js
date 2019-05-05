a4ad99e3b56faaa8135a9c35f5eddaa450a3f654
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-05 18:57:36 -0400

    container event listener that may not stay

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 // Your code goes here
 const navItems = document.querySelectorAll(".nav-link");
-navItems.addEventListener("click", e => {
-  e.preventDefault();
+navItems.forEach(i => {
+  i.addEventListener("click", e => {
+    e.preventDefault();
+  });
 });
+
+const container = document.querySelector('.container.home');
+const letsGoImg = document.querySelector('.letsGo');
+const adventureImg = document.querySelector('.adventureImg');
+
+
+// Random container event listener
+container.style.opacity = '1';
+container.style.transition = '2s';
+
+container.addEventListener('load', e => {
+  e.target.style.opacity = '0';
+})
